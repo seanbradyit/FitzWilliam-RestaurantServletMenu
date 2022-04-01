332f7a133180d6ceeeaf301f1d77f05f9474ff59
seanbradyit <seanbradyit@gmail.com>
2022-04-02 00:00:50 +0100

    Template-Base

--- src/java/RestaurantMenu.java
+++ src/java/RestaurantMenu.java
@@ -1,104 +1,118 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
  */
 /**
- *
  * @author seanb
+ * Sean Brady 2022 - Project 1 - April 1st - First attempt for review...
  */
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.io.*;
-import javax.servlet.annotation.WebServlet;
-import java.text.*;
-import java.sql.*;
-import java.util.Properties;
+import javax.servlet.*; // class imported for servlet & runtime environment communication
+import javax.servlet.http.*; // class imported for servlet & HTTP communication
+import java.io.*; // class imported for standard I/O functionality
+import javax.servlet.annotation.WebServlet; // class imported for servlet declaration and processing
+import java.text.*; // class imported for data processing
+import java.sql.*; // class imported for JDBC API sync and SQL language processing
+import java.util.Properties; // class imported to accomodate stream parameters
 
-@WebServlet(name="RestaurantProject1-Servlet-Menu",urlPatterns={"/RestaurantProject1-Servlet-Menu"})
+@WebServlet(name="RestaurantProject1-Servlet-Menu",urlPatterns={"/RestaurantProject1-Servlet-Menu"}) // servlet name declaration and URL path
 
-public class RestaurantMenu extends HttpServlet {
+public class RestaurantMenu extends HttpServlet { // class created which uses the HttpServlet functionality
     
-    private PreparedStatement Puller;
-    private Connection connection;
+    private PreparedStatement Puller; // container created to contain and transport SQL statement
+    private Connection connection; // object instantiated to establish connection to database
     
-    public void init( ServletConfig config ) throws ServletException
+    public void init( ServletConfig config ) throws ServletException // initialising the servlet and it's configuration 
     {
-        Properties properties = new Properties();
+        Properties properties = new Properties(); // object created to contain and transport parameters
         
-        //properties.put("user","postgres");
-        properties.put("user","root");
-        properties.put("password","admin");
+        properties.put("user","root"); // collecting and attaching username parameter for DB connection
+        properties.put("password","admin"); // collecting and attaching password parameter for DB connection
         
         try{
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver"); // declaring driver for JDBC API
             
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/menu_schema?", properties);
-            Puller = connection.prepareStatement("select * from menu order by id;");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/menu_schema?", properties); // establishing connection to database with credentials
+            Puller = connection.prepareStatement("select * from menu order by id;"); // declaring SQL statement to use
+            //----------------------
+            // 2 minute update to this DB - created in MySQL Workbench via the following... 
+            // CREATE EVENT IF NOT EXISTS datapull ON SCHEDULE EVERY 2 MINUTE DO select * from menu order by id;
+            //----------------------
             
         }catch(Exception e){
-            System.out.println("Exception occured "+e);
+            System.out.println("Exception occured "+e); // catch all for any fault experienced...
         }
     }
     
     protected void doPost( HttpServletRequest request, 
-            HttpServletResponse response)
+            HttpServletResponse response) // prepare the post statement with the servlet and IO exception being thrown and caught...
                 throws ServletException, IOException
     {
-            response.setContentType( "text/html" );
+            response.setContentType( "text/html" ); // declaring the type of content to write
            
-            PrintWriter out = response.getWriter();
-            DecimalFormat twoDigits = new DecimalFormat("0.00");
-            twoDigits.setMaximumFractionDigits(2);
+            PrintWriter out = response.getWriter(); // Output writer for servlet response
+            
+            DecimalFormat twoDigits = new DecimalFormat("0.00"); // creating number presentation style
+            twoDigits.setMaximumFractionDigits(2); // limiting new style to two decimal points only
 
+            // START -- Creating required HTML top portion
             out.println( "<?xml version = \"1.0\"?>");
             out.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD" + 
                     "XHTML 1.0 Strict//EN\" \"http://w3.org" + 
                     "/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
             out.println("<html xmlns = \"http://www.w3.org/1999/xhtml\">");
             out.println("<head>"); 
+            // END -- Creating required HTML top portion
              
             try {
-                ResultSet queue = Puller.executeQuery();
+                ResultSet queue = Puller.executeQuery(); // create queue object to pull and show the results from SQL statement
                 
-                out.println("<title>Thank you!</title>");
+                // START -- Creating required HTML mid portion
+                out.println("<title>Today's Menu</title>");
                 
-                out.println("<style type=\"text/css\">div.a { \n" + "position: absolute;\n" + "left: 150px;\n" + "top: 150px; \n" + "}" + "</style>");
-                out.println("<style type=\"text/css\">div.b { \n" + "position: absolute;\n" + "left: 200px;\n" + "top: 150px; \n" + "}" + "</style>");
+                // ID HTML CSS entry/slot - can be included if required but not in this instance.
+                //out.println("<style type=\"text/css\">div.a { \n" + "position: absolute;\n" + "left: 150px;\n" + "top: 150px; \n" + "}" + "</style>");
+                
+                // Menu Item Description HTML CSS entry/slot
+                out.println("<style type=\"text/css\">div.b { \n" + "position: absolute;\n" + "color: blue;\n" + "left: 200px;\n" + "top: 150px; \n" + "}" + "</style>");
+                
+                // Menu Item Product HTML CSS entry/slot
                 out.println("<style type=\"text/css\">div.c { \n" + "position: absolute;\n" + "left: 350px;\n" + "top: 150px; \n" + "}" + "</style>");
+                
+                // Menu Item Price HTML CSS entry/slot
                 out.println("<style type=\"text/css\">div.d { \n" + "position: absolute;\n" + "left: 450px;\n" + "top: 150px; \n" + "}" + "</style>");
-                out.println("<style type=\"text/css\">div.e { \n" + "position: absolute;\n" + "left: 150px;\n" + "top: 50px; \n" + "}" + "</style>");
                 
                 out.println("</head>");
                 out.println("<body>");
                 out.print("<br>" );
+                // END -- Creating required HTML mid portion
                 
-                StringBuilder idbuilder = new StringBuilder();
+                // String containers to collect each entry
+                // ID can be sourced for display if required... StringBuilder idbuilder = new StringBuilder();
                 StringBuilder pricebuilder = new StringBuilder();
                 StringBuilder descriptionbuilder = new StringBuilder();
                 StringBuilder productbuilder = new StringBuilder();
 
-                        while(queue.next()){
-                                int id = queue.getInt("id");
-                                float pr = queue.getFloat("price");
-                                String desc = queue.getString("description");
-                                String prod = queue.getString("product");
-                                idbuilder.append(id + "<br>");
-                                descriptionbuilder.append(desc + "<br>");
-                                productbuilder.append(prod + "<br>");
-                                pricebuilder.append(twoDigits.format(pr) + "<br>");
+                        while(queue.next()){// While the next entry in the queue is not NULL... Respectively process this entry and...
+                                //int id = queue.getInt("id");
+                                //idbuilder.append(id + "<br>");
+                                float pr = queue.getFloat("price");                // create a local float to store the current value associated with price field
+                                String desc = queue.getString("description");      // create a local string to store the current value associated with description field
+                                String prod = queue.getString("product");          // create a local string to store the current value associated with product field
+                                descriptionbuilder.append(desc + "<br>");          // Append the entry into their respective string builder object
+                                productbuilder.append(prod + "<br>");              // Append the entry into their respective string builder object
+                                pricebuilder.append(twoDigits.format(pr) + "<br>");// Append the entry into their respective string builder object
                         }
                         
-                out.println("<div class=\"a\">ID: <br>" + idbuilder.toString() + "</div>");
-                out.println("<div class=\"b\">Description: <br>" + descriptionbuilder.toString() + "</div>");
-                out.println("<div class=\"c\">Product: <br>" + productbuilder.toString() + "</div>");
-                out.println("<div class=\"d\">Price: <br>" + pricebuilder.toString() + "</div>");
-                out.println("</pre><body></html>");
-                out.close();
+                // START -- Creating required HTML bottom portion
+                //out.println("<div class=\"a\">ID: <br>" + idbuilder.toString() + "</div>");
+                out.println("<div class=\"b\">Description: <br>" + descriptionbuilder.toString() + "</div>"); // show list of product types offered
+                out.println("<div class=\"c\">Product: <br>" + productbuilder.toString() + "</div>"); // show list of individal items offered
+                out.println("<div class=\"d\">Price: <br>" + pricebuilder.toString() + "</div>"); // show list of item prices
+                out.println("</pre><body></html>"); // end HTML doc
+                out.close(); // close I/O post response writer
+                // END -- Creating required HTML bottom portion
             }
             
-            catch(Exception se){
+            catch(Exception se){// Any errors experienced, print out generic respective info for further troubleshooting.
                 se.printStackTrace();
                 System.out.println("Exception occured "+se);
             }
@@ -107,7 +121,7 @@ public class RestaurantMenu extends HttpServlet {
             
     }
     
-    public void destroy()
+    public void destroy() // Method utilised to cleanup all open/lingering established connections...
             {
                 try{
                     Puller.close();
